4871f9efae8ad789038d6b373df4daebaa16caa0
pjd <pranab.deka@kuleuven.be>
2024-11-14 01:25:15 +0100

    Minor correction

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 rm -rf Parareal_Implicit
 
 ### Compile using g++
-g++ ../main.cpp -O3 -fopenmp -I ../../eigen/ -o Parareal_Implicit_2D
+g++ ../main_2D.cpp -O3 -fopenmp -I ../../eigen/ -o Parareal_Implicit_2D
 
 g++ ../main_1D.cpp -O3 -fopenmp -I ../../eigen/ -o Parareal_Implicit_1D
